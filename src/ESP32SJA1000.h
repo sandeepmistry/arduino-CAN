87171a8c54c3047d215bc2ca857981acfe6496c6
Svelbeard <56926365+Svelbeard@users.noreply.github.com>
2023-05-17 22:34:35 +0100

    Move defines from cpp to h file

--- src/ESP32SJA1000.h
+++ src/ESP32SJA1000.h
@@ -11,6 +11,30 @@
 #define DEFAULT_CAN_RX_PIN GPIO_NUM_4
 #define DEFAULT_CAN_TX_PIN GPIO_NUM_5
 
+#define REG_BASE                   0x3ff6b000
+
+#define REG_MOD                    0x00
+#define REG_CMR                    0x01
+#define REG_SR                     0x02
+#define REG_IR                     0x03
+#define REG_IER                    0x04
+
+#define REG_BTR0                   0x06
+#define REG_BTR1                   0x07
+#define REG_OCR                    0x08
+
+#define REG_ALC                    0x0b
+#define REG_ECC                    0x0c
+#define REG_EWLR                   0x0d
+#define REG_RXERR                  0x0e
+#define REG_TXERR                  0x0f
+#define REG_SFF                    0x10
+#define REG_EFF                    0x10
+#define REG_ACRn(n)                (0x10 + n)
+#define REG_AMRn(n)                (0x14 + n)
+
+#define REG_CDR                    0x1F
+
 class ESP32SJA1000Class : public CANControllerClass {
 
 public:
