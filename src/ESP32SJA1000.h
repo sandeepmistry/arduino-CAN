36fddda5285e627df91169e14d4c217f4c287774
Svelbeard <56926365+Svelbeard@users.noreply.github.com>
2023-08-09 19:12:39 +0100

    make register functions public

--- src/ESP32SJA1000.h
+++ src/ESP32SJA1000.h
@@ -69,10 +69,6 @@ protected:
 
   void handleInterrupt();
 
-  uint8_t readRegister(uint8_t address);
-  void modifyRegister(uint8_t address, uint8_t mask, uint8_t value);
-  void writeRegister(uint8_t address, uint8_t value);
-
   static void onInterrupt(void* arg);
 
 protected:
